b2d40e39a767eab4cf890e665e782d3b170b34f3
Luis Valderrama <lfvalderramav@unal.edu.co>
2017-03-28 18:14:32 -0500

    Changed app routing, fix, home appears before anything, routing correctcly to other components.

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { LoginRoutes } from './login/index';
 import { DashboardRoutes } from './dashboard/index';
 
-import { LoginComponent } from './login/index';
+import { DashboardComponent } from './dashboard/index';
 
 export const routes: Routes = [
-	...LoginRoutes,
 	...DashboardRoutes,
-	{ path: '**', component: LoginComponent }
+	{ path: '**', redirectTo: '/dashboard/home',
+    pathMatch: 'full' }
 ];
