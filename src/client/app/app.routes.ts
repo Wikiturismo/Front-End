2089785f939a3b50d901e3c5ab46cccfe1051dfc
Luis Valderrama <lfvalderramav@unal.edu.co>
2017-03-28 19:23:21 -0500

    fix visual recentpost carousel arrows,  deleted unnecesary import from routes

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -2,8 +2,6 @@ import { Routes } from '@angular/router';
 
 import { DashboardRoutes } from './dashboard/index';
 
-import { DashboardComponent } from './dashboard/index';
-
 export const routes: Routes = [
 	...DashboardRoutes,
 	{ path: '**', redirectTo: '/dashboard/home',
