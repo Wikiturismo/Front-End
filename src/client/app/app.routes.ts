f0bbf2ba258cd6f09cf0e7a879d57d375b778971
jmcuestasb <jmcuestasb@unal.edu.co>
2017-03-29 19:05:39 -0500

    ContactUs

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 
+import { LoginRoutes } from './login/index';
 import { DashboardRoutes } from './dashboard/index';
 
+import { LoginComponent } from './login/index';
+
 export const routes: Routes = [
+	...LoginRoutes,
 	...DashboardRoutes,
-	{ path: '**', redirectTo: '/dashboard/home',
-    pathMatch: 'full' }
+	{ path: '**', component: LoginComponent }
 ];
--- src/client/app/dashboard/home/home.module.ts
+++ src/client/app/dashboard/home/home.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { CarouselModule } from 'ng2-bootstrap/ng2-bootstrap';
-import { RecentPostComponent } from './recentpost/recentpost.component';
 
 
 @NgModule({
     imports: [CommonModule, CarouselModule],
-    declarations: [HomeComponent,RecentPostComponent],
+    declarations: [HomeComponent],
     exports: [HomeComponent]
 })
 
