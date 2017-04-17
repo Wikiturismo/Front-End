e6ebda38a5227d7b6958a8d18bfc8bc348c86343
jmcuestasb <jmcuestasb@unal.edu.co>
2017-04-16 20:43:22 -0500

    Actualización Observables

--- src/client/app/dashboard/home/recentpost/recentpost.component.ts
+++ src/client/app/dashboard/home/recentpost/recentpost.component.ts
@@ -15,9 +15,9 @@ import { Wikiservice } from '../../../wiki.service';
 
 export class RecentPostComponent {
 	placesLast;
-	constructor(private wikiservice: Wikiservice) { 
-	
+	constructor(private wikiservice: Wikiservice) {
+
     //this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-	this.wikiservice.getLastPlaces().subscribe(res => this.placesLast = res.json().data);
+	this.wikiservice.getLastPlaces().subscribe(res => this.placesLast = res);
 	}
 }
--- src/client/app/wiki.service.ts
+++ src/client/app/wiki.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-//import { placesLast } from './dashboard/home/recentpost/recentpost.component';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { RecentPostComponent } from './dashboard/home/recentpost/recentpost.component';
+import {Subject} from "rxjs/Subject";
+import {BehaviorSubject} from "rxjs/Rx";
+import {Observable} from 'rxjs/Rx';
+
+
 
 @Injectable()
 export class Wikiservice {
 	//placesLast1;
 	constructor(private http: Http) {
-	
+
 	}
-	getLastPlaces() {
-		return this.http.get('http://localhost:3000/api/v1/places/last.json');
+	getLastPlaces() : Observable<RecentPostComponent[]> {
+		return this.http.get('http://localhost:3000/api/v1/places/last.json')
+		.map(res => (<Response>res).json().data)
+		.catch((error:any) => Observable.throw(error.json().error || 'Server error'));
 		//return this.http.get('http://localhost:3000/api/v1/places/last.json').subscribe(res => this.placesLast1 = res.json().data);
 		//return this.placesLast1;
 	}
